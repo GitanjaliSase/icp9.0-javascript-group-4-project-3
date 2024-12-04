7da07c4c19ef638885ed7e36af2b2bd7de371973
nidhinagpure <nagpurenidhi8@gmail.com>
2024-12-04 15:24:17 +0530

    add text

--- javascript/furniture.js
+++ javascript/furniture.js
@@ -12,3 +12,23 @@
             quantElement.innerText = --quantity;
         }
     }
+
+    function order(){
+        alert('Your order is confirmed...');
+           }
+  
+
+     function furnitureimg(fileName){
+            let img = document.querySelector("#furniture");
+            img.setAttribute("src",fileName)
+        } 
+
+    const stars = document.querySelectorAll(".stars i");
+    stars.forEach((star, index1) => {
+    star.addEventListener("click", () => {
+    
+      stars.forEach((star,index2) => {
+        index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+      });
+    });
+   });
