1ca39b76afc60ea4acd31b8844d6de95a93a59ee
Arpitakharde <arpitakharde30@gmail.com>
2024-12-04 02:13:04 -0800

    display flex property added on box class

--- javascript/kids.js
+++ javascript/kids.js
@@ -55,3 +55,14 @@
     function order(){
       alert('Your order is confirmed...');
          }
+
+ 
+         const stars = document.querySelectorAll(".stars i");
+         stars.forEach((star, index1) => {
+          star.addEventListener("click", () => {
+          
+            stars.forEach((star,index2) => {
+              index1 >= index2 ? star.classList.add("active") : star.classList.remove("active");
+            });
+          });
+         });      
