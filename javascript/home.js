7525c83b08176d58551a540a89ab1dee3b7a92d8
GitanjaliSase <sasegitanjali20@gmail.com>
2024-12-04 14:34:09 +0530

    images change

--- javascript/home.js
+++ javascript/home.js
@@ -3,23 +3,24 @@ const products = [
         "category": " Women Ethnic Wear",
         "items": [
             { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/fashion-page/sadi1.png" },
-            { "name": "Beyond the Match: A Neon Floral Georgette Saree", "price": "Under ₹499", "image": "./images/fashion-page/sadi2.png" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/fashion-page/sadi3.png" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/fashion-page/sadi4.png" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/fashion-page/sadi5.png" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/temple1.jpg" },
+            { "name": "Beyond the Match: A Neon Floral Georgette Saree", "price": "Under ₹1499", "image": "./images/fashion-page/sadi2.png" },
+            { "name": " The Irresistible Allure of Soft Silk Sarees", "price": "From ₹999", "image": "./images/fashion-page/sadi3.png" },
+            { "name": "Floral Printed Puff Sleeves Gathered Flounce Georgette ", "price": "From ₹2699", "image": "./images/fashion-page/sadi4.png" },
+            { "name": "Self Design Bollywood Net Saree", "price": "From ₹1299", "image": "./images/fashion-page/sadi5.png" },
+            { "name": "Self Design Bollywood Net Saree  (Black)Special price", "price": "From ₹1699", "image": "./images/fashion-page/saree2.png" },
 
      ]
     },
     {
+
     "category": " furniture",
         "items": [
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/table5.jpg" },
-            { "name": "Beyond the Match: A Neon Floral Georgette Saree", "price": "Under ₹499", "image": "./images/furniture/studyTable.jpg" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/chair2.jpg" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/bed3.jpg" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/temple1.jpg" },
-            { "name": "Self Design Bollywood Net Saree", "price": "From ₹699", "image": "./images/furniture/temple1.jpg" },
+            { "name": " Bed With Side Pocket Wooden Bedroom Furniture For Home (Natural Finish), 3-Person Sofa, Brown", "price": "From ₹1999", "image": "./images/furniture/bed.jpg" },
+            { "name": "TV Entertainment Unit Set Top Box<br> Stand/Décor Display Unit", "price": "Under ₹11499", "image": "./images/furniture/tvSet.jpg" },
+            { "name": "Home Centre Engineered Wood Montoya<br>6-Seater Dining Set With Chairs", "price": "From ₹1699", "image": "./images/furniture/chairs.jpg" },
+            { "name": " Shoe Rack Cabinet Slipper Footwear Stand", "price": "From ₹2699", "image": "./images/furniture/shoeRack.jpg" },
+            { "name": "Side Table for Bedroom,Bedside Table", "price": "From ₹1699", "image": "./images/furniture/sideTable.jpg" },
+            { "name": "Temple and Office|Set Top Box<br> StandI Wooden Mandir (Pack of 1)", "price": "From ₹9699", "image": "./images/furniture/templeHome.jpg" },
 
      ]
     }
