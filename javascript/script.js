07aabeee94a6f8c83ced47bec10325e1c2235588
NikitaJagtap2003 <jagtapnikita541@gmail.com>
2024-12-04 11:07:14 +0530

    sidebar

--- javascript/script.js
+++ javascript/script.js
@@ -98,4 +98,18 @@ function backward() {
 }
 
 
+    function openSiderbar(){
+        const sidebarElement = document.getElementById('sidebar');
+        sidebarElement.style.visibility = 'visible';
+
+        const hamburgerMenuElement = document.getElementById('hamburger-menu');
+        hamburgerMenuElement.style.visibility = 'hidden'
+    }
+    function closeSidebar(){
+        const sidebarElement = document.getElementById('sidebar');
+        sidebarElement.style.visibility = 'hidden';
+
+        const hamburgerMenuElement = document.getElementById('hamburger-menu');
+        hamburgerMenuElement.style.visibility = 'visible'
+    }
 
