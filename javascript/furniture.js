4474e81a5eb1aa636d2ce89547dea3849e3bb5c9
nidhinagpure <nagpurenidhi8@gmail.com>
2024-12-06 22:53:54 +0530

    change sidebar

--- javascript/furniture.js
+++ javascript/furniture.js
@@ -51,3 +51,32 @@
        sidebar.style.left = '-300px';
    });
    
+   let images = ["./../../images/furniture/furnitures.jpg",
+    "./../../images/furniture/kitchens.jpg", 
+    "./../../images/furniture/homes.jpg",
+     "./../../images/furniture/furnitures.jpg",
+   ];
+let index = 0;
+const slide = document.getElementById('slider');
+
+
+function forward() {
+   if (index < images.length - 1) {
+       index++;
+   }
+   else {
+       index = 0;
+   }
+
+   slide.src = images[index]
+}
+
+function backward() {
+   if (index > 0) {
+       index--;
+   }
+   else {
+       index = images.length - 1;
+   }
+   slide.src = images[index]
+}
